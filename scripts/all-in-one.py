d3af82b040f37eb5621e1ec38dfe0f597958b7a6
PrimedErwin <1973609350@qq.com>
2025-05-18 22:26:13 +0800

    fix dtype

--- scripts/all-in-one.py
+++ scripts/all-in-one.py
@@ -5,7 +5,7 @@ import pandas as pd
 if len(sys.argv) > 1:
     root_path = sys.argv[1]
 else:
-    root_path = '..'
+    root_path = 'U:\Visual Studio\deterministic-math'
 case_result = '/case_result.txt'
 ref_path = '/win32_cl'
 ref_name = 'win32_cl'
@@ -28,14 +28,16 @@ for current_folder in folder_list:
     if current_folder == ref_name:
         continue
     current_ans = pd.read_csv(root_path + '/all-in-one/' + current_folder + case_result, header=None, names=[f'{current_folder}_dec', f'{current_folder}_hex'])
+    replace_i_list = []
     for i in range(ref_row):
         if current_ans.loc[i, f'{current_folder}_hex'] == ref_ans.loc[i, f'{ref_name}_hex']:
-            current_ans.loc[i, f'{current_folder}_dec'] = ''
-            current_ans.loc[i, f'{current_folder}_hex'] = ''
+            replace_i_list.append(i)
     #change column tag and merge decimal/hex
-    current_ans[f'{current_folder}_dec'] = current_ans[f'{current_folder}_dec'] + '(' + current_ans[f'{current_folder}_hex'] + ')'
+    current_ans[f'{current_folder}_dec'] = current_ans[f'{current_folder}_dec'].astype('str') + '(' + current_ans[f'{current_folder}_hex'] + ')'
+    for i in range(len(replace_i_list)):
+        current_ans.loc[replace_i_list[i], f'{current_folder}_dec'] = ''
     current_ans = current_ans.drop(columns=[f'{current_folder}_hex'])
-    current_ans.columns[0] = f'{current_folder}'
+    current_ans = current_ans.rename(columns = {f'{current_folder}_dec': f'{current_folder}'})
     total_ans = pd.concat([total_ans, current_ans], axis=1)
 
 total_ans[f'{ref_name}_dec'] = total_ans[f'{ref_name}_dec'] + '(' + total_ans[f'{ref_name}_hex'] + ')'
